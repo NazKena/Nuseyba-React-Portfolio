dcbcf34b4c57ebf29adb17676309d0b837949304
Nuseyba <n.kenadiid@gmail.com>
2023-03-11 20:02:25 +0000

    started entering code for project.js

--- nuseyba-react-portfolio/src/components/header.js
+++ nuseyba-react-portfolio/src/components/header.js
@@ -3,11 +3,9 @@ import React, { useState } from "react";
 const Header = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleToggle = () => setToggle(!toggle);
-
   return (
     <header>
-      <nav className="hidden md:block">
+      <nav className="hidden">
       <ul>
            <li><a href= "#about-me"><button>About Me</button></a></li>
            <li><a href="#creative-work"><button> Creative Work</button></a></li>
@@ -16,19 +14,8 @@ const Header = () => {
         </ul>
      </nav>
 
-      {/* Mobile Nav */}
-      <nav
-        className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}>
-         <ul>
-           <li><a href= "#about-me"><button>About Me</button></a></li>
-           <li><a href="#creative-work"><button> Creative Work</button></a></li>
-           <li><a href= "#projects"><button>Projects</button></a></li>
-           <li><a href= "#contact-me"><button>Contact Me</button></a></li>
-        </ul>
-     </nav>
-
       {/* Toggle button */}
-      <button onClick={handleToggle} className="block md:hidden">
+      <button onClick={handleToggle} className="hidden">
       </button>
     </header>
   );
---
+++ nuseyba-react-portfolio/src/components/project.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+
+const Projects = () => {
+    const [toggle, setToggle] = useState(false);
+
+return (
+
+<div>
+<div id = "projects">
+  <h2>Projects</h2>
+  <p> Here is screenshots of previous projects</p>  
+  <img src=""></img>
+<img src="./public/img/127.0.0.1_5500_Develop_index.html.jpeg"></img>
+<img src="./n"></img>
+<img src="./"></img>
+<img src="./"></img>
+<img src="./"></img>
+<img src="./"></img>
+
+</div>
+</div> 
+
+)}
+export default Projects;

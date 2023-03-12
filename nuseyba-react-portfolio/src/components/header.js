79f6d8ee3efd60b115e331f210661cf9fc716587
Nuseyba <n.kenadiid@gmail.com>
2023-03-12 15:17:08 +0000

    added styling to about.js

--- nuseyba-react-portfolio/src/components/header.js
+++ nuseyba-react-portfolio/src/components/header.js
@@ -1,23 +1,59 @@
 import React from "react";
-import ProfilePicture from "../img/Nuseyba Kenadid.png"
+import ProfilePicture from "../img/Nuseyba Kenadid.png";
 
+const styles = {
+  container: {
+    width: "30%",
+    borderRadius: "60%",
+  },
 
-function Header () {
+  topnav: {
+    position: "fixed",
+    width: "45%",
+    display: "inline",
+    textAlign: "end",
+    right: "0",
+    zIndex: "99",
+  },
 
-  return (
-    <div class="container">
-    <h1>Nuseyba Kenadid</h1>
-    <img src={ProfilePicture} alt="photo of Nuseyba Kenadid"></img>  
-   <nav class ='topnav'>
-      <ul>
-         <li><a href= "#about-me"><button>About Me</button></a></li>
-         <li><a href="#creative-work"><button> Creative Work</button></a></li>
-         <li><a href= "#projects"><button>Projects</button></a></li>
-         <li><a href= "#contact-me"><button>Contact Me</button></a></li>
-      </ul>
-   </nav>
- </div>
-  );
+  
 };
+
+function Header() {
+  return (
+    <div>
+      <h1>Nuseyba Kenadid</h1>
+      <img
+        src={ProfilePicture}
+        style={styles.container}
+        alt="photo of Nuseyba Kenadid"
+      ></img>
+      <nav style={styles.topnav}>
+        <ul>
+          <li>
+            <a href="#about-me">
+              <button>About Me</button>
+            </a>
+          </li>
+          <li>
+            <a href="#creative-work">
+              <button> Creative Work</button>
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              <button>Projects</button>
+            </a>
+          </li>
+          <li>
+            <a href="#contact-me">
+              <button>Contact Me</button>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default Header;

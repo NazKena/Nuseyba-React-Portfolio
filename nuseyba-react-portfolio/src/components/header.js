273ad2c7829813fef1ef21ae463a6a1226131550
Nuseyba <n.kenadiid@gmail.com>
2023-03-12 14:55:45 +0000

    imported image into Reacr

--- nuseyba-react-portfolio/src/components/header.js
+++ nuseyba-react-portfolio/src/components/header.js
@@ -1,20 +1,22 @@
 import React from "react";
+import ProfilePicture from "../img/Nuseyba Kenadid.png"
+
 
 function Header () {
 
   return (
-    <header>
-      <nav className="hidden">
+    <div class="container">
+    <h1>Nuseyba Kenadid</h1>
+    <img src={ProfilePicture} alt="photo of Nuseyba Kenadid"></img>  
+   <nav class ='topnav'>
       <ul>
-           <li><a href= "#about-me"><button>About Me</button></a></li>
-           <li><a href="#creative-work"><button> Creative Work</button></a></li>
-           <li><a href= "#projects"><button>Projects</button></a></li>
-           <li><a href= "#contact-me"><button>Contact Me</button></a></li>
-        </ul>
-     </nav>
-
-
-    </header>
+         <li><a href= "#about-me"><button>About Me</button></a></li>
+         <li><a href="#creative-work"><button> Creative Work</button></a></li>
+         <li><a href= "#projects"><button>Projects</button></a></li>
+         <li><a href= "#contact-me"><button>Contact Me</button></a></li>
+      </ul>
+   </nav>
+ </div>
   );
 };
 
--- nuseyba-react-portfolio/src/components/project.js
+++ nuseyba-react-portfolio/src/components/project.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+function Projects () {
+return (
+
+    <div>
+    <h2>Creative Work</h2>
+    <p>Here I will inclde both my services as well as outlets that serve to showcase my creative skills.</p>  
+    <img src='./'></img>  
+ </div>
+
+);
+};
+
+export default Projects;

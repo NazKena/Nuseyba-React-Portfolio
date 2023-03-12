c182e671595b842ff584aaf657a0f4309f328485
Nuseyba <n.kenadiid@gmail.com>
2023-03-12 13:59:59 +0000

    about.js rendering successfully

--- nuseyba-react-portfolio/src/App.js
+++ nuseyba-react-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from "./components/header";
+import About from './components/about';
 
 function App() {
 
   return (
     <>
     <Header/>
+    <About/>
+
     </>
   )
   }
--- nuseyba-react-portfolio/src/components/about.js
+++ nuseyba-react-portfolio/src/components/about.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div> 
-  )
-}
+
+    <div class="container1">
+    <section id="about-me" class="circle"></section>
+       <h2> About Me</h2>
+       <p>I am an English and Creative Writing graduate who has recently found that a different career path may be exactly what is needed to spark creativity again. Coding will allow for that and so much more. I hope to enhance my skills not only throughout the duration of this bootcamp but through the entirety of my software development journey.</p>
+</div>  
+)}
+
 export default About

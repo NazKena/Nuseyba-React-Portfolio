865ba2541deb1dde6ca020e525051d59b8dc3e52
Nuseyba <n.kenadiid@gmail.com>
2023-03-12 16:48:44 +0000

    added images to projects.js

--- nuseyba-react-portfolio/src/App.js
+++ nuseyba-react-portfolio/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from "./components/header";
 import About from './components/about';
-import Footer from "./components/footer"
+import Footer from "./components/footer";
+import Projects from './components/project';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <About/>
     <Footer/>
+    <Projects/>
    </>
   )
   }
--- nuseyba-react-portfolio/src/components/project.js
+++ nuseyba-react-portfolio/src/components/project.js
@@ -1,27 +1,60 @@
 import React, { useState } from "react";
-import NoteTakerImg from "../img/Screenshot 2023-03-07 at 19.52.44.png"
+import NoteTakerImg from "../img/Screenshot 2023-03-07 at 19.52.44.png";
+import PassGenImg from "../img/127.0.0.1_5500_Develop_index.html.jpeg";
+import ProfileGenImg from "../img/Screenshot 2023-03-01 at 13.32.34.png";
+import VimGalleryImg from "../img/Screenshot 2023-03-11 at 16.47.01.png";
 
-function Projects () {
+const styles = {
+  row: {
+    display: "flex",
+    flexDirection: "row",
+  },
 
-const [developers] = useState ([
-    {
-        image:{NoteTakerImg},
-        name: NoteTaker,
-        application:"https://herokus-note-taker.herokuapp.com/",
-        github: "https://github.com/NazKena/Note_Taker",
-    }
-]
-) 
-
-return (
-
-    <div>
-    <h2>Creative Work</h2>
-    <p>Here I will inclde both my services as well as outlets that serve to showcase my creative skills.</p>  
-    <img src="./"></img>
- </div>
-
-);
+  creativeWork: {
+    width: "30%",
+    borderRadius: "1%",
+  },
 };
+
+function Projects() {
+  return (
+    <div id="creative-work">
+      <h2>Creative Work</h2>
+      <p>
+        Here I will inclde both my services as well as outlets that serve to
+        showcase my creative skills.
+      </p>
+      <div class="row">
+        <h3>Note Taker</h3>
+        <img
+          src={NoteTakerImg}
+          style={styles.creativeWork}
+          alt="photo of Nuseyba Kenadid"
+        ></img>
+
+        <h3>Password Generator</h3>
+        <img
+          src={PassGenImg}
+          style={styles.creativeWork}
+          alt="photo of Nuseyba Kenadid"
+        ></img>
+
+        <h3>Vim Gallery</h3>
+        <img
+          src={VimGalleryImg}
+          style={styles.creativeWork}
+          alt="photo of Nuseyba Kenadid"
+        ></img>
+
+        <h3>Team Profile Generator</h3>
+        <img
+          src={ProfileGenImg}
+          style={styles.creativeWork}
+          alt="photo of Nuseyba Kenadid"
+        ></img>
+      </div>
+    </div>
+  );
+}
 
 export default Projects;

f6aac790f1b4afae1b04a582336cc32b2b2fbb49
Nuseyba <n.kenadiid@gmail.com>
2023-03-13 20:51:36 +0000

    changes

--- nuseyba-react-portfolio/src/App.js
+++ nuseyba-react-portfolio/src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/header";
 import About from "./components/about";
 import Projects from "./components/project";
 import Contact from "./components/contact";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 
 
@@ -10,12 +10,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+    <Router basename="nuseyba-react-portfolio">
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact/>} />
       </Routes>
+    </Router>  
     </div>
   );
 }
